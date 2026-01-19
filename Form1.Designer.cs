a9345df9e3aa7eaacc654b26e88d6f68084f6b05
Brigita <brigitaalenskaite@gmail.com>
2026-01-19 11:57:55 +0200

    SD2 WinForms GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SD2
+{
+    partial class SD2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.failasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.atidarytiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.išsaugotiĮvestįToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.išsaugotiRezultatusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.uždarytiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apieProgramąToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.cmbMode = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.failasToolStripMenuItem,
+            this.apieToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // failasToolStripMenuItem
+            // 
+            this.failasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.atidarytiToolStripMenuItem,
+            this.išsaugotiĮvestįToolStripMenuItem,
+            this.išsaugotiRezultatusToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.uždarytiToolStripMenuItem});
+            this.failasToolStripMenuItem.Name = "failasToolStripMenuItem";
+            this.failasToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+            this.failasToolStripMenuItem.Text = "Failas";
+            // 
+            // atidarytiToolStripMenuItem
+            // 
+            this.atidarytiToolStripMenuItem.Name = "atidarytiToolStripMenuItem";
+            this.atidarytiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.atidarytiToolStripMenuItem.Text = "Atidaryti";
+            this.atidarytiToolStripMenuItem.Click += new System.EventHandler(this.atidarytiToolStripMenuItem_Click);
+            // 
+            // išsaugotiĮvestįToolStripMenuItem
+            // 
+            this.išsaugotiĮvestįToolStripMenuItem.Name = "išsaugotiĮvestįToolStripMenuItem";
+            this.išsaugotiĮvestįToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.išsaugotiĮvestįToolStripMenuItem.Text = "Išsaugoti įvestį";
+            this.išsaugotiĮvestįToolStripMenuItem.Click += new System.EventHandler(this.išsaugotiĮvestįToolStripMenuItem_Click);
+            // 
+            // išsaugotiRezultatusToolStripMenuItem
+            // 
+            this.išsaugotiRezultatusToolStripMenuItem.Name = "išsaugotiRezultatusToolStripMenuItem";
+            this.išsaugotiRezultatusToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.išsaugotiRezultatusToolStripMenuItem.Text = "Išsaugoti rezultatus";
+            this.išsaugotiRezultatusToolStripMenuItem.Click += new System.EventHandler(this.išsaugotiRezultatusToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(221, 6);
+            // 
+            // uždarytiToolStripMenuItem
+            // 
+            this.uždarytiToolStripMenuItem.Name = "uždarytiToolStripMenuItem";
+            this.uždarytiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.uždarytiToolStripMenuItem.Text = "Uždaryti";
+            this.uždarytiToolStripMenuItem.Click += new System.EventHandler(this.uždarytiToolStripMenuItem_Click);
+            // 
+            // apieToolStripMenuItem
+            // 
+            this.apieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.apieProgramąToolStripMenuItem});
+            this.apieToolStripMenuItem.Name = "apieToolStripMenuItem";
+            this.apieToolStripMenuItem.Size = new System.Drawing.Size(54, 26);
+            this.apieToolStripMenuItem.Text = "Apie";
+            // 
+            // apieProgramąToolStripMenuItem
+            // 
+            this.apieProgramąToolStripMenuItem.Name = "apieProgramąToolStripMenuItem";
+            this.apieProgramąToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.apieProgramąToolStripMenuItem.Text = "Apie programą";
+            this.apieProgramąToolStripMenuItem.Click += new System.EventHandler(this.apieProgramąToolStripMenuItem_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 31);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(711, 109);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(22, 163);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(766, 171);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // cmbMode
+            // 
+            this.cmbMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMode.FormattingEnabled = true;
+            this.cmbMode.Location = new System.Drawing.Point(22, 405);
+            this.cmbMode.Name = "cmbMode";
+            this.cmbMode.Size = new System.Drawing.Size(121, 24);
+            this.cmbMode.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(260, 395);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(129, 43);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Skaičiuoti";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // SD2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cmbMode);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SD2";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem failasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem atidarytiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem išsaugotiĮvestįToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem išsaugotiRezultatusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem uždarytiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem apieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem apieProgramąToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ComboBox cmbMode;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SD2
+{
+    public partial class SD2 : Form
+    {
+        public SD2()
+        {
+            InitializeComponent();
+
+            cmbMode.Items.Clear();
+            cmbMode.Items.Add("Vidurkis");
+            cmbMode.Items.Add("Mediana");
+            cmbMode.Items.Add("Abu");
+            cmbMode.SelectedIndex = 0;
+
+            txtOutput.ReadOnly = true;
+        }
+
+        private void atidarytiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtInput.Text = File.ReadAllText(openFileDialog1.FileName, Encoding.UTF8);
+            }
+        }
+
+        private void išsaugotiĮvestįToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, txtInput.Text, Encoding.UTF8);
+            }
+        }
+
+        private void išsaugotiRezultatusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, txtOutput.Text, Encoding.UTF8);
+            }
+        }
+
+        private void uždarytiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void apieProgramąToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Studentų pažymių skaičiavimo programa (SD2)\nKūrėja: Jūsų vardas pavardė\nOrganizacija: VVK",
+                "Apie programą",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var students = ParseStudents(txtInput.Text);
+
+                if (students.Count == 0)
+                {
+                    txtOutput.Text = "Nėra duomenų skaičiavimui.";
+                    return;
+                }
+
+                foreach (var s in students)
+                {
+                    s.FinalAvg = 0.4 * s.Homeworks.Average() + 0.6 * s.Exam;
+                    s.FinalMed = 0.4 * Median(s.Homeworks) + 0.6 * s.Exam;
+                }
+
+                students = students
+                    .OrderBy(x => x.Surname, StringComparer.CurrentCulture)
+                    .ThenBy(x => x.Name, StringComparer.CurrentCulture)
+                    .ToList();
+
+                txtOutput.Text = BuildOutput(students, cmbMode.SelectedIndex);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Klaida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private class Student
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public List<int> Homeworks { get; set; }
+            public int Exam { get; set; }
+            public double FinalAvg { get; set; }
+            public double FinalMed { get; set; }
+        }
+
+        private static List<Student> ParseStudents(string text)
+        {
+            var lines = text
+                .Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(l => l.Trim())
+                .Where(l => l.Length > 0)
+                .ToList();
+
+            if (lines.Count == 0) return new List<Student>();
+
+            bool header = lines[0].Contains("ND") || lines[0].Contains("Egz") || lines[0].Contains("Egz.");
+            if (header) lines.RemoveAt(0);
+
+            var res = new List<Student>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length < 4) continue;
+
+                string name = parts[0];
+                string surname = parts[1];
+
+                var nums = parts.Skip(2)
+                    .Select(p =>
+                    {
+                        if (!int.TryParse(p, NumberStyles.Integer, CultureInfo.InvariantCulture, out int v))
+                            throw new Exception("Neteisingas skaičius eilutėje:\n" + line);
+                        return v;
+                    })
+                    .ToList();
+
+                if (nums.Count < 2)
+                    throw new Exception("Trūksta ND/egzamino reikšmių eilutėje:\n" + line);
+
+                int egz = nums.Last();
+                var nd = nums.Take(nums.Count - 1).ToList();
+
+                if (nd.Count == 0)
+                    throw new Exception("Trūksta namų darbų reikšmių eilutėje:\n" + line);
+
+                res.Add(new Student
+                {
+                    Name = name,
+                    Surname = surname,
+                    Homeworks = nd,
+                    Exam = egz
+                });
+            }
+
+            return res;
+        }
+
+        private static double Median(List<int> v)
+        {
+            var a = v.OrderBy(x => x).ToList();
+            int n = a.Count;
+            if (n % 2 == 1) return a[n / 2];
+            return (a[n / 2 - 1] + a[n / 2]) / 2.0;
+        }
+
+        private static string BuildOutput(List<Student> students, int mode)
+        {
+            var sb = new StringBuilder();
+
+            if (mode == 0) sb.AppendLine("Pavarde\tVardas\tGalutinis(Vid.)");
+            else if (mode == 1) sb.AppendLine("Pavarde\tVardas\tGalutinis(Med.)");
+            else sb.AppendLine("Pavarde\tVardas\tGalutinis(Vid.)\tGalutinis(Med.)");
+
+            sb.AppendLine(new string('-', 60));
+
+            foreach (var s in students)
+            {
+                if (mode == 0)
+                    sb.AppendLine($"{s.Surname}\t{s.Name}\t{s.FinalAvg:F2}");
+                else if (mode == 1)
+                    sb.AppendLine($"{s.Surname}\t{s.Name}\t{s.FinalMed:F2}");
+                else
+                    sb.AppendLine($"{s.Surname}\t{s.Name}\t{s.FinalAvg:F2}\t{s.FinalMed:F2}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
